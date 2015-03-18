62dccf8c6194029c626f796f9a348dc318353273
Marcel Dierkes <marcel.dierkes@goodscloud.com>
2015-03-18 09:40:59 +0100

    Removed DDLogger dependency added a method to read the HockeyAppID.plist from the cordova www folder.

--- src/ios/HockeyAppPlugin.m
+++ src/ios/HockeyAppPlugin.m
@@ -5,16 +5,11 @@
 #import <HockeySDK/HockeySDK.h>
 
 #import "HockeyAppPlugin.h"
-#import "DDFileLogger.h"
-
-extern int ddLogLevel;
 
 static NSString *const kHockeyAppPluginAppReachedTerminateEventKey = @"AppReachedTerminateEvent";
 
 @interface HockeyAppPlugin ()
-
-@property (nonatomic) DDFileLogger *fileLogger;
-
+@property (copy, nonatomic) NSString *hockeyAppID;
 @end
 
 @implementation HockeyAppPlugin
@@ -22,16 +17,8 @@ static NSString *const kHockeyAppPluginAppReachedTerminateEventKey = @"AppReache
 #pragma mark Initialization
 
 - (void)pluginInitialize {
-    NSString * hockeyAppKey = @"__HOCKEY_APP_KEY__";
-    if( hockeyAppKey!=nil && [hockeyAppKey isEqualToString:@""]==NO && [hockeyAppKey rangeOfString:@"HOCKEY_APP_KEY"].location == NSNotFound ){
-
-        // initialize before HockeySDK, so the delegate can access the file logger
-        self.fileLogger = [[DDFileLogger alloc] init];
-        self.fileLogger.maximumFileSize = (1024 * 64); // 64 KByte
-        self.fileLogger.logFileManager.maximumNumberOfLogFiles = 1;
-        [self.fileLogger rollLogFileWithCompletionBlock:nil];
-        [DDLog addLogger:self.fileLogger];
-
+    NSString *hockeyAppKey = self.hockeyAppID;
+    if(hockeyAppKey && ![hockeyAppKey isEqualToString:@""]) {
         // hack to prevent sending crash report for crashes that occur when app is shutting down
         // reference for deleting crash report file:
         // http://support.hockeyapp.net/discussions/problems/33370-is-there-a-way-to-manually-send-the-most-recent-crash-report-after-changing-the-crash-reporting-status
@@ -51,20 +38,17 @@ static NSString *const kHockeyAppPluginAppReachedTerminateEventKey = @"AppReache
             NSError *error = nil;
             if (fileExists) {
                 if (![fileManager removeItemAtPath:filePath error:&error]) {
-                    DDLogInfo(@"Could not delete crash report file:  %@ (%@)", error, filePath);
+					NSLog(@"Could not delete crash report file:  %@ (%@)", error, filePath);
                 }
             }
         }
         
-        [[BITHockeyManager sharedHockeyManager] configureWithBetaIdentifier:hockeyAppKey
-                                                             liveIdentifier:hockeyAppKey
-                                                                   delegate:self];
-        
+        [[BITHockeyManager sharedHockeyManager] configureWithIdentifier:hockeyAppKey];
         [[BITHockeyManager sharedHockeyManager] startManager];
         [[BITHockeyManager sharedHockeyManager].authenticator authenticateInstallation];
     }
     
-    DDLogInfo(@"HockeyApp Plugin initialized");
+    NSLog(@"HockeyApp Plugin initialized");
 }
 
 #pragma mark Shutdown methods
@@ -77,50 +61,35 @@ static NSString *const kHockeyAppPluginAppReachedTerminateEventKey = @"AppReache
 #pragma mark Plugin methods
 
 - (void)forcecrash:(CDVInvokedUrlCommand *)command {
-  DDLogWarn(@"HockeyApp Plugin crashing the app!");
+  NSLog(@"HockeyApp Plugin crashing the app!");
   __builtin_trap();
 }
 
-#pragma mark Logging methods
+#pragma mark - Hockey app identifier methods
 
-// get the log content with a maximum byte size
-- (NSString *) getLogFilesContentWithMaxSize:(NSInteger)maxSize {
-    NSMutableString *description = [NSMutableString string];
-    
-    NSArray *sortedLogFileInfos = [[_fileLogger logFileManager] sortedLogFileInfos];
-    NSInteger count = [sortedLogFileInfos count];
-    
-    // we start from the last one
-    for (NSInteger index = count - 1; index >= 0; index--) {
-        DDLogFileInfo *logFileInfo = [sortedLogFileInfos objectAtIndex:index];
-        
-        NSData *logData = [[NSFileManager defaultManager] contentsAtPath:[logFileInfo filePath]];
-        if ([logData length] > 0) {
-            NSString *result = [[NSString alloc] initWithBytes:[logData bytes]
-                                                        length:[logData length]
-                                                      encoding: NSUTF8StringEncoding];
-            
-            [description appendString:result];
+- (NSString *)hockeyAppID {
+    if(!_hockeyAppID) {
+        _hockeyAppID = [self hockeyAppIDFromPropertyList];
+    }
+    return _hockeyAppID;
+}
+
+- (NSString *)hockeyAppIDFromPropertyList {
+    // Get current bundle identifier
+	NSBundle *bundle = [NSBundle mainBundle];
+	NSString *bundleIdentifier = bundle.infoDictionary[(__bridge NSString *)kCFBundleIdentifierKey];
+	
+	// Load the matching plist from HockeyAppID.plist
+    NSString *path = [bundle pathForResource:@"HockeyAppID" ofType:@"plist" inDirectory:@"www"];
+    if(path) {
+        NSDictionary *dictionary = [NSDictionary dictionaryWithContentsOfFile:path];
+        if(dictionary) {
+            NSString *appID = dictionary[bundleIdentifier];
+            return appID;
         }
     }
     
-    if ([description length] > maxSize) {
-        description = (NSMutableString *)[description substringWithRange:NSMakeRange([description length]-maxSize-1, maxSize)];
-    }
-    
-    return description;
-}
-
-
-#pragma mark - BITCrashManagerDelegate
-
-- (NSString *)applicationLogForCrashManager:(BITCrashManager *)crashManager {
-    NSString *description = [self getLogFilesContentWithMaxSize:5000]; // 5000 bytes should be enough!
-    if ([description length] == 0) {
-        return nil;
-    } else {
-        return description;
-    }
+    return nil;
 }
 
 @end
